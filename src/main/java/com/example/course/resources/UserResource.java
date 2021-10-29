d5a1d0e2da8b033f889b4e220fa47d1ed797d52a
juniorn06 <juniorn06@gmail.com>
2021-10-28 22:44:16 -0300

    User entity and resource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.example.course.resources;
+
+import com.example.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
