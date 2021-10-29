74c12ab76f25d1ddd9e8273ddab667c56729abb3
Junior <juniorn06@gmail.com>
2021-10-29 16:00:51 -0300

    Service layer, component registration

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -2,9 +2,7 @@ package com.example.course.repositories;
 
 import com.example.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
